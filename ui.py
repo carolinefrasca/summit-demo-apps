ccadc576a56781b8495d24f9968b066ce053ca48
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-06-07 14:09:38 -0400

    Update ui.py

--- ui.py
+++ ui.py
@@ -143,14 +143,14 @@ def colored_header(label, color=None, description=None):
 
 
 # From: https://discuss.streamlit.io/t/href-on-image/9693/3
-@st.cache(allow_output_mutation=True)
+@st.cache_data()
 def get_base64_of_bin_file(bin_file):
     with open(bin_file, "rb") as f:
         data = f.read()
     return base64.b64encode(data).decode()
 
 
-@st.cache(allow_output_mutation=True)
+@st.cache_data()
 def get_img_with_href(local_img_path, target_url):
     img_format = os.path.splitext(local_img_path)[-1].replace(".", "")
     bin_str = get_base64_of_bin_file(local_img_path)
