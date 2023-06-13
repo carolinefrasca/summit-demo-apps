5430f3bbe1337dd3aac2ffa9fec99b6be5a2566c
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-06-13 14:17:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -280,7 +280,7 @@ with col1:
     )
 with col2:
     app(
-        "Ask the Oracle of Omaha",
+        "The Oracle of Omaha",
         "Quickly generate a PDF file using data collected from user input.",
         "images/Oracle.png",
         "https://jrpettus-streamlit-buffett-buffett-app-hqw5pq.streamlit.app/",
